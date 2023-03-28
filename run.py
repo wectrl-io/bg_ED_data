80341a78907343dbadfd787dc1c0f2161160df49
maslyankov <maslyankov@users.noreply.github.com>
2023-03-28 21:24:46 +0300

    Revert "Move electrohold to oop code"

--- run.py
+++ run.py
@@ -1,52 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
+import electrohold.prices as el_prices
+import electrohold.outages as el_outages
 
-from providers.electrohold.electrohold import Electrohold as Provider
-
-#region File Attributes
-
-__author__ = "Orlin Dimitrov"
-"""Author of the file."""
-
-__copyright__ = ""
-"""Copyrighted"""
-
-__credits__ = []
-"""Credits"""
-
-__license__ = ""
-"""License
-@see """
-
-__version__ = "1.0.0"
-"""Version of the file."""
-
-__maintainer__ = ["Orlin Dimitrov", "Martin Maslyankov", "Nikola Atanasov"]
-"""Name of the maintainer."""
-
-__email__ = ""
-"""E-mail of the author."""
-
-__class_name__ = "BaseProvider"
-"""Provider class name."""
-
-#endregion
 
 def main():
-
     print("Starting work...")
 
-    provider = Provider()
-    ids = ['300066244165', '123456789101']
+    check_id = 123456789101
 
-    for identifier in ids:
-        print("Electricity outages data:")
-        print(provider.get_outages(identifier))
+    print("Electricity outages data:")
+    print(el_outages.get_outages(check_id))
 
     print("Electricity prices data:")
-    print(provider.get_prices())
+    print(el_prices.get_prices())
 
     print("Finitto!")
+
+
 
 if __name__ == "__main__":
     main()
