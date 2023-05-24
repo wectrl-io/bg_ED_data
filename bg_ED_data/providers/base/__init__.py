bd589a73f187387791ae27857321cd1882c4c0c0
Orlin Dimitrov <orlin369@gmail.com>
2023-05-24 22:37:21 +0300

    Fix init files

--- bg_ED_data/providers/base/__init__.py
+++ bg_ED_data/providers/base/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
 # -*- coding: utf8 -*-
 
 #region File Attributes
@@ -30,6 +29,3 @@ __class_name__ = ""
 """Provider class name."""
 
 #endregion
-=======
-# -*- coding: utf8 -*-
->>>>>>> c74953c12589b0b65d342d681e8c0850f9341ed9
